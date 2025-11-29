f83a54ac28fc4b6d047b9a7dcb6afe489ebdd822
Gabriel Beraldi <gabriel.beraldi@hotmail.com>
2025-11-28 21:48:19 -0400

    fix: adjuts mikro-orm to read props from .env

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,23 +1,22 @@
 import { defineConfig, UnderscoreNamingStrategy } from "@mikro-orm/core";
 import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
-  host: 'localhost',
-  port: 5433,
-  user: 'postgres',
-  password: 'postgres',
-  dbName: 'gestao_projetos_db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
   driver: PostgreSqlDriver,
-
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
-
   migrations: {
     path: 'dist/migrations',
     pathTs: 'src/migrations',
   },
-
   namingStrategy: UnderscoreNamingStrategy,
-
   // metadataProvider: TsMorphMetadataProvider
 })
